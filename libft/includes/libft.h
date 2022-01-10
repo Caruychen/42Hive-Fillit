6c933229dcc0d3e64409158e8ee716e8a4be17f7
Caruy Chen <cchen@student.hive.fi>
2022-01-10 16:25:51 +0200

    initialise with libft

---
+++ libft/includes/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/01 13:39:10 by cchen             #+#    #+#             */
+/*   Updated: 2022/01/06 14:24:23 by cchen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include "ft_ctype.h"
+# include "ft_math.h"
+# include "ft_stdio.h"
+# include "ft_stdlib.h"
+# include "ft_string.h"
+# include "list.h"
+# include "vec.h"
+
+// BONUS
+#endif
