4603f2f5b698627b2d88f1a12e993e8e42ab5382
Caruy Chen <caruychen@gmail.com>
2022-01-12 12:53:31 +0200

    added verification logic

--- src/fillit.h
+++ src/fillit.h
@@ -6,13 +6,13 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 13:34:12 by cchen             #+#    #+#             */
-/*   Updated: 2022/01/11 13:35:14 by cchen            ###   ########.fr       */
+/*   Updated: 2022/01/12 12:01:47 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
 
-int	read_block(char *filename);
+int	read_inputs(char *filename);
 
 #endif
--- src/reader.c
+++ src/reader.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 13:25:52 by cchen             #+#    #+#             */
-/*   Updated: 2022/01/11 17:05:40 by cchen            ###   ########.fr       */
+/*   Updated: 2022/01/12 12:53:11 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,47 @@
 #include "get_next_line.h"
 #include "ft_stdlib.h"
 
-static int	valid_char(char c)
+static int	valid_char(char *piece, int index)
 {
-	return (c == '#' || c == '.');
+	char	c;
+
+	c = piece[index];
+	if (index % 5 < 4)
+		return (c == '#' || c == '.');
+	return (c == '\n');
 }
 
-static int	valid_line(char *line)
+static int	valid_blocks(char *piece, int index)
 {
-	unsigned int	index;
+	static unsigned int	vertices;
+	static unsigned int	blocks;
+
+	if (index == 0)
+	{
+		vertices = 0;
+		blocks = 0;
+	}
+}
+
+static int	valid_piece(char *piece, int bytes)
+{
+	int	index;
 
 	index = 0;
-	while (line[index])
+	while (index < 20)
 	{
-		if (index == 4 || !valid_char(line[index]))
+		if (!(valid_char(piece, index)))
+			return (0);
+		if (!(valid_blocks(piece, index)))
 			return (0);
 		++index;
 	}
-	if (index < 3)
+	if (bytes == 21 && piece[index] != '\n')
 		return (0);
 	return (1);
 }
 
-static long	read_block(int fd, char *buff, long *bytes)
+static long	read_piece(int fd, char *buff, long *bytes)
 {
 	long	ret;
 
@@ -46,33 +65,33 @@ static long	read_block(int fd, char *buff, long *bytes)
 	{
 		ret = read(fd, buff + *bytes, 21 - *bytes);
 		if (ret < 0)
-			return (ret);
+			return (*bytes = ret);
 		if (ret == 0)
 			break;
-		bytes += ret;
+		*bytes += ret;
 	}
 	return (*bytes);
 }
 
-static int	get_next_block(int fd)
+static int	get_next_piece(int fd)
 {
-	char	*buff[21];
+	char	buff[21];
 	long	bytes;
 
 	bytes = 0;
-	if (read_block(fd, &buff, bytes) < 0)
+	if (read_piece(fd, buff, &bytes) <= 0)
+		return (bytes);
+	if (!valid_piece(buff, (int) bytes))
 		return (-1);
-	if (
-	
 	return (bytes);
 }
 
-int	read_block(char *filename)
+int	read_inputs(char *filename)
 {
 	int		fd;
 
 	fd = open(filename, O_RDONLY);
-	while (get_next_block(fd) > 0)
+	while (get_next_piece(fd) > 0)
 	{
 		ft_putendl("valid");
 	}
