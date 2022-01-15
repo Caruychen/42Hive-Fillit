fb65d26622bb7395ead985a3c459b77c93ba9efa
Caruychen <caruychen@gmail.com>
2022-01-15 23:39:23 +0200

    Added structs for pieces and points

--- src/fillit.h
+++ src/fillit.h
@@ -6,14 +6,30 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 13:34:12 by cchen             #+#    #+#             */
-/*   Updated: 2022/01/12 16:44:43 by cchen            ###   ########.fr       */
+/*   Updated: 2022/01/15 22:51:20 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
 
-# define TET_SIZE 20
+# define TET_BUFF 20
+
+typedef struct s_piece
+{
+	uint64_t	barray;
+	uint8_t		letter;
+	uint8_t		x;
+	uint8_t		y;
+	uint8_t		width;
+	uint8_t		height;
+}	t_piece;
+
+typedef struct s_point
+{
+	uint8_t		x;
+	uint8_t		y;
+}	t_point;
 
 int	valid_piece(char *piece, int bytes);
 int	read_input(char *filename);
