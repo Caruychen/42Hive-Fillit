e2993bcf3b2c2340f2d894494d86a3470ff5230d
Caruychen <caruychen@gmail.com>
2022-01-17 09:26:15 +0200

    added pieces paramter to read_input

--- src/main.c
+++ src/main.c
@@ -6,20 +6,24 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 11:44:20 by cchen             #+#    #+#             */
-/*   Updated: 2022/01/12 15:39:37 by cchen            ###   ########.fr       */
+/*   Updated: 2022/01/17 09:25:10 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include "ft_stdio.h"
+#include "ft_string.h"
 
 int	main(int argc, char **argv)
 {
+	t_piece		pieces[MAX_PIECES + 1];
+
 	if (argc != 2)
 	{
 		ft_putendl("usage: ./fillit source_file");
 		return (1);
 	}
-	read_input(argv[1]);
+	ft_bzero(pieces, sizeof(t_piece) * (MAX_PIECES + 1));
+	read_input(argv[1], pieces);
 	return (0);
 }
