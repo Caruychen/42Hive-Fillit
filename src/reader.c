17ec4e00052d16c0551db99e0b02b23cd5617f49
Caruy Chen <caruychen@gmail.com>
2022-01-18 14:55:47 +0200

    update reader

--- src/reader.c
+++ src/reader.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 13:25:52 by cchen             #+#    #+#             */
-/*   Updated: 2022/01/17 13:15:11 by cchen            ###   ########.fr       */
+/*   Updated: 2022/01/18 13:52:49 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,22 +52,23 @@ static int	reading(int fd, char *buff, int *count)
 	ret = (int) get_next_piece(fd, buff);
 	if (ret < 0 || *count == 26)
 		return (*count = -1);
-	*count += ret;
 	return (ret);
 }
 
-int	read_input(char *filename, t_piece *pieces)
+int	read_input(char *filename, t_piece *pieces, int *count)
 {
 	int		fd;
-	int		count;
 	char	buff[TET_BUFF + 1];
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (-1);
-	count = 0;
-	while (reading(fd, buff, &count) > 0)
-		pieces[count] = build_piece(buff, count);
+	*count = 0;
+	while (reading(fd, buff, count) > 0)
+	{
+		pieces[*count] = build_piece(buff, *count);
+		++*count;
+	}
 	close(fd);
-	return (count);
+	return (*count);
 }
